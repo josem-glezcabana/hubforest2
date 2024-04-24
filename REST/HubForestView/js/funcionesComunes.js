fd1bf49e271c025dc660c3f3495a6122d070d5d5
José Manuel González Cabana <josemcabana9@gmail.com>
2024-04-24 22:41:14 +0200

    Primera versión vista proyectos
    - Lista de proyectos, con botones de añadir, buscar, editar y eliminar
    - Falta multiidioma y vista según el rol
    - Estaría bien un scroll o acotar la altura del modal al añadir proyecto

--- REST/HubForestView/js/funcionesComunes.js
+++ REST/HubForestView/js/funcionesComunes.js
@@ -77,6 +77,7 @@ function includeHeader() {
                                 '</button>' +
                                 '<ul class="dropdown-menu">' +
                                     '<li><a class="dropdown-item" href="listaUsuarios.html">Usuarios</a></li>' +
+                                    '<li><a class="dropdown-item" href="proyectos.html">Proyectos</a></li>' +
                                 '</ul>' +
                             '</div>' +
                         '</li>' +
