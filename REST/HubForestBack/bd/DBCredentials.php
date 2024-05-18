64f1163ce64a3dbf121b5061c1478cf5c3b94c51
José Manuel González Cabana <josemcabana9@gmail.com>
2024-05-18 17:53:07 +0200

    Añadido comentario de host para uso con instalador: en caso de probar con instalador, comentar el de "mariadb" y descomentar "127.0.01". Para docker, al revés

--- REST/HubForestBack/bd/DBCredentials.php
+++ REST/HubForestBack/bd/DBCredentials.php
@@ -1,5 +1,8 @@
 <?php
+// host para ejecución con docker
 define('host', 'mariadb');
+// host para ejecución con instalador
+// define('host', '127.0.0.1');
 define('user', 'hubForestUser');
 define('pass', 'hubForestUserPass');
 define('BD', 'hubforest');
