fb41a04b6bdf3b01a3f9297d9108f82c65a216b1
Iagosevic <micromachine13@hotmail.com>
2024-05-18 19:45:17 +0200

    Add files via upload

---
+++ REST/HubForestBack/app/temporal_sampling_site_params/temporal_sampling_site_params_CONTROLLER.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once './Base/ControllerBase.php';
+
+class temporal_sampling_site_params extends ControllerBase{
+
+
+	function __construct(){
+		parent::__construct();
+	}
+
+
+} //FIN DE CLASS
+?>
---
+++ REST/HubForestBack/app/temporal_sampling_site_params/temporal_sampling_site_params_SERVICE.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once './Base/appServiceBase.php';
+
+class temporal_sampling_site_params_SERVICE extends appServiceBase{
+
+	public $modelo;
+
+	//METODOS
+
+	function __construct(){
+
+		parent::__construct();
+
+	}
+
+	function inicializarRest(){
+
+		$this->listaAtributos = array('id_project','id_ecosystem','id_ecosystem_param','category_param','name_ecosystem_param','values_ecosystem_param');
+
+		$this->listaAtributosSelect = array('id_project','id_ecosystem','id_ecosystem_param','category_param','name_ecosystem_param','values_ecosystem_param');
+
+		$this->notnull = array(
+						'ADD' => array('id_project','id_ecosystem','id_ecosystem_param','category_param','name_ecosystem_param'),
+						'EDIT' => array('id_project','id_ecosystem','id_ecosystem_param','category_param','name_ecosystem_param'),
+						'DELETE' => array('id_project','id_ecosystem','id_ecosystem_param')
+						);
+
+		$this->modelo = $this->crearModelOne('temporal_sampling_site_params');
+
+	}
+
+}
